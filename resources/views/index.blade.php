8a7814ec85645e01db29ff46d40b10cbabb84c7a
David Juan Ahullana <info@davidjotta.me>
2016-07-26 04:13:45 +0200

    Nice version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
 @include ('_header')
 
 <div class="container">
-  <div class="row content">
+  <div class="row">
     <div class="col-lg-8 col-lg-offset-2">
-      <h1>Soon.</h1>
+      <a class="logo" href="mailto:info@maymaxstudio.com">
+        <img src="/img/logo_white.png">
+      </a>
       <p>
         <a class="btn btn-cta btn-lg" href="https://play.google.com/apps/testing/com.maymaxstudio.hundredjumps" target="_blank">HUNDRED JUMPS</a>
       </p>
