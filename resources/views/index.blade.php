1f4062ec8405f2ba70462b926a6273d48c670216
David Juan Ahullana <info@davidjotta.me>
2016-07-26 02:59:23 +0200

    First page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,14 @@
 @include ('_header')
 
-<h1>It works!</h1>
+<div class="container">
+  <div class="row centered">
+    <div class="col-lg-8 col-lg-offset-2 mt">
+      <h1>Soon.</h1>
+      <p class="mt">
+        <a class="btn btn-cta btn-lg" href="https://play.google.com/apps/testing/com.maymaxstudio.hundredjumps" target="_blank">HUNDRED JUMPS</a>
+      </p>
+    </div>
+  </div>
+</div>
 
 @include ('_footer')
