9fbac795200826ccf1faa9675d171d0ae3ec96e4
David Juan Ahullana <info@davidjotta.me>
2016-07-26 03:20:20 +0200

    Fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 @include ('_header')
 
 <div class="container">
-  <div class="row centered">
-    <div class="col-lg-8 col-lg-offset-2 mt">
+  <div class="row content">
+    <div class="col-lg-8 col-lg-offset-2">
       <h1>Soon.</h1>
-      <p class="mt">
+      <p>
         <a class="btn btn-cta btn-lg" href="https://play.google.com/apps/testing/com.maymaxstudio.hundredjumps" target="_blank">HUNDRED JUMPS</a>
       </p>
     </div>
